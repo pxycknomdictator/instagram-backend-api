7e8cf1032d9b7c99cf2b97430df29707effe5ca1
Noman <pxycknomdictator@outlook.com>
2025-04-24 16:50:02 +0500

    added users model types in codebase

---
+++ src/types/user.types.ts
@@ -0,0 +1,24 @@
+import { Schema } from "mongoose";
+
+enum Gender {
+  male = "male",
+  female = "female",
+  others = "others",
+}
+
+interface UserSchema extends Document {
+  _id: Schema.Types.ObjectId;
+  username: string;
+  name: string;
+  avatar: string;
+  website: string;
+  bio: string;
+  gender: Gender;
+  posts: Schema.Types.ObjectId[];
+  followers: Schema.Types.ObjectId[];
+  following: Schema.Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export { Gender, UserSchema };
--- src/utils/asyncGuard.ts
+++ src/utils/asyncGuard.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { Fn } from "../types/types.js";
+import { Fn } from "../types/asyncGuard.types.js";
 
 export const asyncGuard =
   (fn: Fn) => async (req: Request, res: Response, next: NextFunction) => {
