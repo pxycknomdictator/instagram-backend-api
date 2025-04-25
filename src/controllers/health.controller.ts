237fafc1fd50a6d40dfd6aa0fac643a756a29133
Noman <pxycknomdictator@outlook.com>
2025-04-25 17:30:00 +0500

    added asyncGuard in health check controller

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { ApiRes } from "../utils/response.js";
+import { asyncGuard } from "../utils/asyncGuard.js";
 
-export async function healthCheck(_: Request, res: Response) {
-  res.status(200).json(new ApiRes(200, "Server is Running..."));
-}
+export const healthCheck = asyncGuard(async (_: Request, res: Response) => {
+  return res.status(200).json(new ApiRes(200, "Server is Running..."));
+});
