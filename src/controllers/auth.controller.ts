1e7eab22281eeebc19f38db10c87e6495dd2f6b1
Noman <pxycknomdictator@outlook.com>
2025-04-25 16:52:53 +0500

    added user login controller in codebase

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,8 +1,12 @@
+import * as argon2 from "argon2";
+import jwt from "jsonwebtoken";
+
 import { User } from "../models/user.model.js";
 import { ApiRes } from "../utils/response.js";
 import { asyncGuard } from "../utils/asyncGuard.js";
 import { hashPassword } from "../helpers/password.helper.js";
-import { RegisterSchema } from "../validators/user.validator.js";
+import { LoginSchema, RegisterSchema } from "../validators/user.validator.js";
+import { configs } from "../constant.js";
 
 const register = asyncGuard(async (req, res) => {
   // Don't worry bro you used middleware for body testing
@@ -34,7 +38,32 @@ const register = asyncGuard(async (req, res) => {
 });
 
 const login = asyncGuard(async (req, res) => {
-  res.status(200).json(new ApiRes(200, "Login", { name: "Noman" }));
+  // Don't worry bro you used middleware for body testing
+  const { email, password }: LoginSchema = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) return res.status(404).json(new ApiRes(404, "Credentials Error"));
+
+  const isPasswordCorrect = await argon2.verify(user.password, password);
+  if (!isPasswordCorrect) {
+    return res.status(404).json(new ApiRes(404, "Credentials Error"));
+  }
+
+  const accessToken = jwt.sign(
+    { _id: user._id, username: user.username, email: user.email },
+    configs.JWT_ACCESS_TOKEN_SECRET_KEY!,
+  );
+
+  const refreshToken = jwt.sign(
+    { _id: user._id },
+    configs.JWT_ACCESS_TOKEN_SECRET_KEY!,
+  );
+
+  await User.findByIdAndUpdate(user._id, { refreshToken }, { new: true });
+
+  res.cookie("access_token", accessToken).cookie("refresh_token", refreshToken);
+
+  return res.status(200).json(new ApiRes(200, "Login"));
 });
 
 export { register, login };
