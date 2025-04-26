236d487e1f5e93fba6e3b6215710d36625979a5f
Noman <pxycknomdictator@outlook.com>
2025-04-26 13:15:15 +0500

    added a renew token routes for renewing tokens

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,10 +1,17 @@
+import jwt from "jsonwebtoken";
 import { ApiRes } from "../utils/response.js";
 import { User } from "../models/user.model.js";
 import { asyncGuard } from "../utils/asyncGuard.js";
 import { tokensGenerator } from "../helpers/token.helper.js";
-import { cookiesOptions, access_token, refresh_token } from "../constant.js";
+import {
+  cookiesOptions,
+  access_token,
+  refresh_token,
+  configs,
+} from "../constant.js";
 import { decodePassword, hashPassword } from "../helpers/password.helper.js";
 import { LoginSchema, RegisterSchema } from "../validators/user.validator.js";
+import { ObjectId } from "mongoose";
 
 const register = asyncGuard(async (req, res) => {
   // Don't worry bro you used middleware for body testing
@@ -90,4 +97,56 @@ const logout = asyncGuard(async (req, res) => {
     .json(new ApiRes(200, "Logout"));
 });
 
-export { register, login, logout };
+const renewTokens = asyncGuard(async (req, res) => {
+  const incomingRefreshToken =
+    req.body?.refresh_token || req.cookies?.access_token;
+
+  if (!incomingRefreshToken) {
+    return res.status(401).json(new ApiRes(401, "Token is Required"));
+  }
+
+  type DecodedTokenPayload = { _id: string };
+
+  let decoded: DecodedTokenPayload;
+
+  try {
+    decoded = jwt.verify(
+      incomingRefreshToken,
+      configs.JWT_REFRESH_TOKEN_SECRET_KEY!,
+    ) as DecodedTokenPayload;
+  } catch (error) {
+    return res.status(400).json(new ApiRes(400, "Invalid or Expired Token"));
+  }
+
+  const user = await User.findOne({
+    $and: [{ _id: decoded._id }, { refreshToken: incomingRefreshToken }],
+  })
+    .select("-password")
+    .lean();
+
+  if (!user) {
+    return res.status(404).json(new ApiRes(404, "User not found"));
+  }
+
+  const [accessToken, refreshToken] = tokensGenerator({
+    _id: user?._id as ObjectId,
+    username: user?.username as string,
+    email: user?.email as string,
+  });
+
+  await User.findByIdAndUpdate(user?._id, { $set: { refreshToken } });
+
+  return res
+    .cookie(access_token, accessToken, {
+      ...cookiesOptions,
+      maxAge: 1000 * 60 * 60 * 24,
+    })
+    .cookie(refresh_token, refreshToken, {
+      ...cookiesOptions,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    })
+    .status(200)
+    .json(new ApiRes(200, "Tokens are Renewed"));
+});
+
+export { register, login, logout, renewTokens };
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { validate } from "../middlewares/validate.middleware.js";
 import { validateAuth } from "../middlewares/auth.middleware.js";
-import { register, login, logout } from "../controllers/auth.controller.js";
 import { loginSchema, registerSchema } from "../validators/user.validator.js";
+import {
+  register,
+  login,
+  logout,
+  renewTokens,
+} from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.post("/signup", validate(registerSchema), register);
 router.post("/login", validate(loginSchema), login);
 router.get("/logout", validateAuth, logout);
+router.post("/renew-tokens", renewTokens);
 
 export default router;
