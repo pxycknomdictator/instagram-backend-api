ebbe2cfdc1f7e5ccf5429163507a5fd399f97bbe
Noman <pxycknomdictator@outlook.com>
2025-04-26 18:00:11 +0500

    added a user profile route for getting user by its username

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,21 @@
+import { ApiRes } from "../utils/response.js";
+import { User } from "../models/user.model.js";
+import { asyncGuard } from "../utils/asyncGuard.js";
+
+const getUser = asyncGuard(async (req, res) => {
+  const username = req.params?.username;
+
+  if (!username) {
+    return res.status(400).json(new ApiRes(400, "username is required"));
+  }
+
+  const user = await User.findOne({ username }).select(
+    "-password -refreshToken -email",
+  );
+
+  if (!user) return res.status(404).json(new ApiRes(404, "User not found"));
+
+  return res.status(200).json(new ApiRes(200, "User Information", user));
+});
+
+export { getUser };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getUser } from "../controllers/user.controller.js";
 
 const router = Router();
+
+router.get("/profile/:username", getUser);
 
 export default router;
