3c2f95e358aa0500b60c54d1bea3b899817ad1c8
Noman <pxycknomdictator@outlook.com>
2025-04-27 20:10:53 +0500

    fixed create post route schema validation error

--- src/validators/post.validator.ts
+++ src/validators/post.validator.ts
@@ -1,17 +1,12 @@
 import { z } from "zod";
-import { isValidObjectId } from "mongoose";
-import { PostType } from "../types/post.types.js";
 
-const postSchema = z.object({
-  createdBy: z.string().refine((id) => isValidObjectId(id), {
-    message: "Invalid user id",
-  }),
-  title: z.string().optional(),
-  description: z.string().optional(),
-  postType: z.enum([PostType.image, PostType.video]),
-  postUrl: z.string({ message: "post url is required" }).trim(),
-  private: z.boolean().optional(),
-});
+const postSchema = z
+  .object({
+    title: z.string().optional(),
+    description: z.string().optional(),
+    private: z.boolean().optional(),
+  })
+  .optional();
 
 type PostSchema = z.infer<typeof postSchema>;
 
