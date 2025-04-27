8c9a1ddd61277378d6ef3f9a6145cbbef57a8b50
Noman <pxycknomdictator@outlook.com>
2025-04-27 18:29:49 +0500

    added getPosts controller in codebase

---
+++ src/controllers/post.controller.ts
@@ -0,0 +1,10 @@
+import { ApiRes } from "../utils/response.js";
+import { Post } from "../models/post.model.js";
+import { asyncGuard } from "../utils/asyncGuard.js";
+
+const getPosts = asyncGuard(async (_, res) => {
+  const posts = await Post.find();
+  return res.status(200).json(new ApiRes(200, "Posts", posts));
+});
+
+export { getPosts };
--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { getPosts } from "../controllers/post.controller.js";
 
 const router = Router();
+
+router.route("/").get(getPosts);
 
 export default router;
