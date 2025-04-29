0ae16e108934727250e26d37e0ce14b4e77bbe2d
Noman <pxycknomdictator@outlook.com>
2025-04-29 12:45:13 +0500

    removed unnecessary imports from codebase

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,7 +1,6 @@
-import { Request, Response } from "express";
 import { ApiRes } from "../utils/response.js";
 import { asyncGuard } from "../utils/asyncGuard.js";
 
-export const healthCheck = asyncGuard(async (_: Request, res: Response) => {
+export const healthCheck = asyncGuard(async (_, res) => {
   return res.status(200).json(new ApiRes(200, "Server is Running..."));
 });
