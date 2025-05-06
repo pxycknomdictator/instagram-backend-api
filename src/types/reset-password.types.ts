f8bb47d25bbb084f0fb3e2cc6f0fa1e25d9b922a
Noman <pxycknomdictator@outlook.com>
2025-05-06 18:03:35 +0500

    added password reset types in app

---
+++ src/types/reset-password.types.ts
@@ -0,0 +1,12 @@
+import { Schema, Document } from "mongoose";
+
+interface PasswordReset extends Document {
+  _id: Schema.Types.ObjectId;
+  userId: Schema.Types.ObjectId;
+  passwordResetCode: string;
+  expireAt: Date;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export { PasswordReset };
