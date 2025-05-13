553efc0a695faa86cbc4d815207817b4e72c6e5d
Noman <pxycknomdictator@outlook.com>
2025-05-14 01:01:22 +0500

    added message model in codebase

---
+++ src/models/message.model.ts
@@ -0,0 +1,13 @@
+import { Schema, model } from "mongoose";
+import { MessageSchema } from "../types/message.types.js";
+
+const messageSchema = new Schema<MessageSchema>(
+  {
+    sender: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    receiver: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    message: { type: String, required: true },
+  },
+  { timestamps: true },
+);
+
+export const Message = model<MessageSchema>("Message", messageSchema);
